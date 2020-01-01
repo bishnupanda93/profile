6b2425a6f0a7cb1e1d5015839d632083ed74bfcd
Priya <guptapriyab@gmail.com>
2020-01-02 00:24:09 +0100

    Added responsiveness

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,15 @@ import classes from './Header.module.scss';
 const Header = (props)=>{
     return(
         <div className={[classes.header, 'row pb-4 px-5 pt-5'].join(" ")}>
-            <div className="col-8 d-flex flex-column-reverse">
+            <div className="col-sm-8 col-12 d-flex flex-column-reverse">
             <h1>{props.personDetails.name}</h1>
             </div>
-            <div className="col-4">
+            <div className="col-sm-4 col-12">
                 <ul>
-                    <li>{props.personDetails.address} <i class="fa fa-home" aria-hidden="true"></i></li>
-                    <li>{props.personDetails.phone}</li>
-                    <li>{props.personDetails.email}</li>
-                    <li>{props.personDetails.linkedIn}></li>
+                    <li>{props.personDetails.address} <i className="fa fa-home" aria-hidden="true"></i></li>
+                    <li>{props.personDetails.phone} <i className="fa fa-phone"></i></li>
+                    <li>{props.personDetails.email}<i className="fa fa-envelope"></i></li>
+                    <li><a href={props.personDetails.linkedIn}>LinkedIn<i className="fa fa-linkedin"></i></a></li>
                 </ul>
             </div>
         </div>
