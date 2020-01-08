3640c690271b287aedd4922df1472b4d6f666cc6
Priya <guptapriyab@gmail.com>
2020-01-08 23:35:19 +0100

    Subtitle resposive issue solved

--- src/components/Portfolio/SubTitle/SubTitle.js
+++ src/components/Portfolio/SubTitle/SubTitle.js
@@ -4,11 +4,12 @@ const SubTitle =props=>{
     const wrapper = [classes['subtitle-wrapper'],'row no-gutters'];
     return(
     <div className={wrapper.join(" ")}>
-        <div className="col-auto">
+        <div className="col col-sm-auto">
             <span className= {classes['bold-green-text']}>{props.role}</span>
         </div>
         <div className="d-none d-sm-flex">&nbsp;|&nbsp;</div>
-        <div className="col-auto">
+        <div className="w-100 d-flex d-sm-none"></div>
+        <div className="col col-sm-auto">
         <span className={classes['muted-grey-text']}>{props.organisation}</span>
         </div>
     </div>
