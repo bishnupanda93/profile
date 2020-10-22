8001498e5cb12f67305c462d6817702981ea0999
Priya <guptapriyab@gmail.com>
2020-10-22 20:53:01 +0530

    Updates

---
+++ precache-manifest.578f5c26ee7545f6d4fd7b660cd5cc92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1eca64226903b0ea7515273fc42efc",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "1fc27426a858d42407e8",
+    "url": "/profile/static/css/main.30aed83f.chunk.css"
+  },
+  {
+    "revision": "3cc11f4bdaeb91d5788a",
+    "url": "/profile/static/js/2.bde109af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/profile/static/js/2.bde109af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1fc27426a858d42407e8",
+    "url": "/profile/static/js/main.9a4b3cf4.chunk.js"
+  },
+  {
+    "revision": "cc6b9f377aed1b32f17b",
+    "url": "/profile/static/js/runtime-main.788cbbec.js"
+  }
+]);
