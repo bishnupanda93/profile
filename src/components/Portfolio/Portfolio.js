edf263a0f0c0b594b322dd54d0a68fdb121a7a62
Bishnu Prasad Panda <65193676+bishnupanda93@users.noreply.github.com>
2020-11-09 20:24:59 +0530

    Update Portfolio.js

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -31,14 +31,14 @@ const Portfolio =(props)=>{
             </div>
             <div className="mt-4 pt-2">
                 <Title value="Experience" icon="fa fa-briefcase"></Title>
-                <SubTitle organisation="Tata Consultancy Services" role="Frontend Developer"></SubTitle>
-                <Tenure startDate="AUG 2015" endDate="PRESENT"></Tenure>
-                <p className={classes['summary-text']}>IT Analyst at TCS and following are accounts/clients’ projects that I have worked upon:</p>
+               <SubTitle organisation="Tata Consultancy Services" role="Automation Tester"></SubTitle>
+                <Tenure startDate="Mar 2016" endDate="PRESENT"></Tenure>
+                <p className={classes['summary-text']}>Functional Analyst at TCS and following are accounts/clients’ projects that I have worked upon:</p>
                 {experience}
             </div>
             <div className="mt-4 pt-2">
                 <Title value="Education" icon ="fa fa-university"></Title>
-                <SubTitle organisation="University of Mumbai" role="Bachelor of Engineering"></SubTitle>
+                <SubTitle organisation="Biju Pattnaik University of Technology, Odisha" role="Bachlor in Technology"></SubTitle>
                 <Tenure startDate="2011" endDate="2015"></Tenure>
             </div>
         </div>
